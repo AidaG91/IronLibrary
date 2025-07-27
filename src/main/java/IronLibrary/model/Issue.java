06ad003303972eaaa307b2dea2f52386a20c4919
krub-dev <kikorubioillan@gmail.com>
2025-07-27 03:49:43 +0200

    added Issue Class to implement

--- src/main/java/IronLibrary/model/Issue.java
+++ src/main/java/IronLibrary/model/Issue.java
@@ -1,5 +1,69 @@
 package IronLibrary.model;
 
 public class Issue {
+    private int issueId; // Autoincrement
+    private String issueDate;
+    private String returnDate;
+    private Student issueStudent;
+    private Book issueBook;
 
+    // Constructor
+    public Issue(String issueDate, String returnDate, Student issueStudent, Book issueBook) {
+        this.issueDate = issueDate;
+        this.returnDate = returnDate;
+        this.issueStudent = issueStudent;
+        this.issueBook = issueBook;
+    }
+
+    // Getters & Setters
+
+    public int getIssueId() {
+        return issueId;
+    }
+
+    public void setIssueId(int issueId) {
+        this.issueId = issueId;
+    }
+
+    public String getIssueDate() {
+        return issueDate;
+    }
+
+    public void setIssueDate(String issueDate) {
+        this.issueDate = issueDate;
+    }
+
+    public String getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(String returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public Student getIssueStudent() {
+        return issueStudent;
+    }
+
+    public void setIssueStudent(Student issueStudent) {
+        this.issueStudent = issueStudent;
+    }
+
+    public Book getIssueBook() {
+        return issueBook;
+    }
+
+    public void setIssueBook(Book issueBook) {
+        this.issueBook = issueBook;
+    }
+
+    // Methods
+
+
+    // Info data toString()
+    @Override
+    public String toString() {
+        return "Issue: " + issueBook.getTitle() + " to " + issueStudent.getName() +
+                " from " + issueDate + " to " + returnDate;
+    }
 }
