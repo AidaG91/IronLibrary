8f69a457090b0662aa4a425b17cd46e3dcf77083
Carol-88 <gralleta@gmail.com>
2025-08-07 13:44:43 +0200

    marcado

--- src/test/java/IronLibrary/model/IssueTest.java
+++ src/test/java/IronLibrary/model/IssueTest.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.*;
 
 class IssueTest {
-
+    //REFERENCIA
     private Issue issue;
     private Student student;
     private Book book;
